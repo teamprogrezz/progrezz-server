deaff296f9882dfb762a46eb8d8e7142f828b9ec
Wikiti <wikiti.doghound@gmail.com>
2014-12-14 11:38:04 +0000

    Añadiendo sistema de actualización de mensajes de fragmentos.

--- rb/game/api/messages/user_messages_uris.rb
+++ rb/game/api/messages/user_messages_uris.rb
@@ -3,6 +3,8 @@ require 'sinatra/jsonp'
 require 'json'
 require 'geocoder'
 
+
+# Recibir mensajes cercanos al usuario
 before '/game/dba/geoloc' do
   content_type 'application/javascript'
 end
@@ -35,6 +37,8 @@ get '/game/dba/geoloc' do
   return params[:callback] + "(" + output.to_json() + ")"
 end
 
+
+# Recibir lista de mensajes de un usuario
 before '/game/dba/msg_user_list' do
   content_type 'application/javascript'
 end
@@ -77,3 +81,32 @@ get '/game/dba/msg_user_list' do
 
   return params[:callback] + "(" + output.to_json + ")"
 end 
+
+# Actualizar fragmentos encontrados
+before '/game/dba/msg_user_get_fragment' do
+  content_type 'application/javascript'
+end
+
+get '/game/dba/msg_user_get_fragment' do
+  # Comprobaciones iniciales: comprobar que el mensaje y el usuario existe.
+  # ...
+
+  output = {}
+
+  # Comprobar que el mensaje no esta completado y el fragmento no se ha recogido antes.
+  if   Game::Database::UserCompletedMessages.get(params['id_user'], params['id_msg']) != nil ||
+       Game::Database::UsserMessages.get(params['id_user'], params['id_msg'], params['fragment_index'])) != nil
+
+    return params[:callback] + "(" + output.to_json + ")"
+  end
+
+  # Añadir el mensaje a la base de datos de la lista de fragmentos de un usuario
+  user_fragment = Game::Database::UsserMessages.new
+  user_fragment.id_user = params['id_user']
+  user_fragment.id_msg  = params['id_msg']
+  user_fragment.fragment_index  = params['fragment_index']
+  
+  # Actualizar mensajes completados del usuario para un id de mensaje dado
+  # ...
+end
+
