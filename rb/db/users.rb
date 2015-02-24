1fa2574261d3e451a2b485b3bb0f4075556526da
Daniel Herzog <wikiti.doghound@gmail.com>
2015-02-24 10:26:54 +0000

    Añadido rakefile sencillo, además de hacer pruebas con la creación de usuarios.

--- rb/db/users.rb
+++ rb/db/users.rb
@@ -8,16 +8,23 @@ module Game
       include Neo4j::ActiveNode
       
       # -------------------------
-      #        Atributos
+      #      Atributos (DB)
       # -------------------------
-      property :user_id # Identificador de usuario
-      property :alias   # Sin usar.
+      property :user_id, constraint: :unique  # Identificador de usuario (correo, único)
+      property :alias                         # Sin usar.
 
       # -------------------------
-      #         Métodos
+      #    Métodos de clase
       # -------------------------
-      # ...
+      def self.sign_in(al, uid)
+        begin
+          return create( {alias: al, user_id: uid} );
+        rescue
+          raise "DB ERROR: Cannot create user '" + al + "': '" + uid + "' is in use.";
+        end
+
+      end
+
     end
-
   end
 end
--- rb/game/api/test/trash_test.rb
+++ rb/game/api/test/trash_test.rb
@@ -2,13 +2,16 @@ require 'sinatra'
 require 'neo4j'
 
 get '/test/t1' do
-  result = "hola"
-  test_user = Game::Database::User.create( {alias: 'prueba', user_id: 'id'} )
-
-  all_users = Game::Database::User.all()
-  all_users.each do |user|
-    result +=  "<br>"
+  begin
+    test_user = Game::Database::User.sign_in('Wikiti', 'wikiti.doghound@gmail.com' )
+    return "Hello, " + test_user.alias + " (" + test_user.user_id + ")";
+    #all_users = Game::Database::User.all()
+    #all_users.each do |user|
+    #  result +=  "<br>"
+    #end
+  rescue Exception => e
+    return e.message
   end
- 
-  return result;
+
+  result += "<br>" + test_user.class.name
 end
