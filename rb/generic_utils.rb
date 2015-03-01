b7ceddbf8d8bce43a3e0eadb4ef9c5c8c3751597
Daniel Herzog <wikiti.doghound@gmail.com>
2015-03-01 21:27:16 +0000

    Modificado código para ejecutar cualquier tipo de script.

--- rb/generic_utils.rb
+++ rb/generic_utils.rb
@@ -30,22 +30,38 @@ class GenericUtils
     return Time.now - pre_time
   end
   
-  # Ejecuta un programa python.
+  # Ejecuta un programa o script cualquiera.
   #
   # * *Argumentos*:
-  #   - +script_file+: Fichero del script python.
-  #   - +input_str+: Cadena de entrada sustituyendo al flujo STDIN.
+  #   - +executable+: Ejecutable o intérprete a lanzar (ejemplo: python, ruby, ls, executable_cpp, ...).
+  #   - +arguments+:  Argumentos a pasar al ejecutable (ejemplo: file.py, main.rb, -la, ...).
+  #   - +input_str+:  Cadena de entrada sustituyendo al flujo STDIN.
   #
   # * *Devuelve* 
-  #   - Salida estándar del script del flujo STDOUT.
+  #   - STDOUT y STDERR del script, como un hash, tal que {stdout: STDOUT, stderr: STDERR}.
   #
-  def self.run_py(script_file, input_str)
-    cmd = 'python ' + script_file
+  def self.run_script(executable, arguments, input_str = "")
+    cmd = executable + " " + arguments
     
     return Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
       stdin.puts input_str
       return { stdout: stdout.read, stderr: stderr.read }
     end
+  end
+  
+  # Ejecuta un programa python.
+  #
+  # Es un acceso directo al método GenericUtils.run_script con el intérprete de python.
+  #
+  # * *Argumentos*:
+  #   - +script_file+: Fichero del script python.
+  #   - +input_str+:   Cadena de entrada sustituyendo al flujo STDIN.
+  #
+  # * *Devuelve* 
+  #   - STDOUT y STDERR del script, como un hash, tal que {stdout: STDOUT, stderr: STDERR}.
+  #
+  def self.run_py(script_file, input_str = "")
+    return GenericUtils.run_script('python', script_file, input_str)
   end
 
   #-- ...  #++
