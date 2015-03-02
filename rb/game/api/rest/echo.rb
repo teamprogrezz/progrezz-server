1a0f7551877feaa2c2e6d30a4c30da2e090b857b
Daniel Herzog <wikiti.doghound@gmail.com>
2015-03-02 09:51:02 +0000

    Añadidas funciones echo y echo_py de la API REST a un nuevo fichero.

---
+++ rb/game/api/rest/echo.rb
@@ -0,0 +1,49 @@
+#:nodoc:
+module Sinatra
+  module API
+  module REST
+    class Methods
+      # Redireccionar a página interactiva
+      def self.test(app, response)
+        app.redirect to("/dev/api/rest/interactive")
+      end
+
+      # Método de prueba (saludar al nombre "request.data.name" de la petición realizada).
+      def self.echo(app, response)
+        begin
+          if response[:request][:data] == nil || response[:request][:data][:name] == nil
+            name = 'world'
+          else
+            name = response[:request][:data][:name].to_s
+          end
+          
+          response[:response][:data][:type]    = "plain"
+          response[:response][:data][:message] = "Hello, " + name + "!"
+        rescue Exception => e
+          raise "Invalid request: " + e.message
+        end
+      end
+
+      # Método de prueba usando python (saludar al nombre "request.data.name" de la petición realizada).
+      def self.echo_py(app, response)
+        begin
+          if response[:request][:data] == nil || response[:request][:data][:name] == nil
+            name = 'world'
+          else
+            name = response[:request][:data][:name].to_s
+          end
+          
+          input_json = '{"name": "' + name + '"}'
+          
+          response[:response][:data][:type]    = "plain"
+          response[:response][:data][:message] = GenericUtils.run_py('scripts/python/echo.py', { "INPUT_JSON" => input_json })[:stdout] 
+        rescue Exception => e
+          raise "Invalid request: " + e.message
+        end
+
+      end
+      # ...
+    end
+  end
+  end
+end
--- rb/game/api/rest/rest.rb
+++ rb/game/api/rest/rest.rb
@@ -9,42 +9,6 @@ module Sinatra
       def self.test(app, response)
         app.redirect to("/dev/api/rest/interactive")
       end
-
-      # Método de prueba (saludar al nombre "request.data.name" de la petición realizada.
-      def self.echo(app, response)
-        begin
-          if response[:request][:data] == nil || response[:request][:data][:name] == nil
-            name = 'world'
-          else
-            name = response[:request][:data][:name].to_s
-          end
-          
-          response[:response][:data][:type]    = "plain"
-          response[:response][:data][:message] = "Hello, " + name + "!"
-        rescue Exception => e
-          raise "Invalid request: " + e.message
-        end
-      end
-
-      def self.echo_py(app, response)
-        begin
-          if response[:request][:data] == nil || response[:request][:data][:name] == nil
-            name = 'world'
-          else
-            name = response[:request][:data][:name].to_s
-          end
-          
-          input_json = '{"name": "' + name + '"}'
-          
-          response[:response][:data][:type]    = "plain"
-          response[:response][:data][:message] = GenericUtils.run_py('scripts/python/echo.py', { "INPUT_JSON" => input_json })[:stdout] 
-        rescue Exception => e
-          raise "Invalid request: " + e.message
-        end
-        
-        
-      end
-      # ...
     end
 
     def self.registered(app)
