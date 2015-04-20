794c5c19d44c977c2181b4102208a120ebbe35b9
Daniel Herzog <wikiti.doghound@gmail.com>
2015-04-20 12:15:13 +0100

    Refactorizado más codigo. Añadido cambio en tiempo real de los parámetros del juego.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,5 @@
 function toggle_spoiler(spoiler_id) {
-  element = document.getElementById(element);
+  var element = document.getElementById(spoiler_id);
   
   if (element.style.display == "none") {
     element.style.display = "";
